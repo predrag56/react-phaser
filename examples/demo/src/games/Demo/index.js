6776d9940e587dcb81f1e66c84ef323e99f13bb3
Firanolfind <firanolfind@gmail.com>
2019-10-23 15:26:45 +0200

    fix demo scene

--- examples/demo/src/games/Demo/index.js
+++ examples/demo/src/games/Demo/index.js
@@ -23,7 +23,6 @@ var config = {
 };
 
 const assets = [
-	['atlas', 'coins', 'media/coins.png', 'media/coins.json'],
 	['atlas', 'gem', 'media/gems.png', gemsJson],
 	['atlas', 'flares', 'media/flares.png', flares],
 	['audio', 'background', 'media/music.mp3'],
@@ -40,7 +39,6 @@ class DemoGame extends Component {
 	};
 
 	handleGem = () => {
-		console.log('clicked!');
 		this.setState(({ x, y }) => ({
 			x: x === 60 ? 400 : 60,
 			y: y === 10 ? 400 : 10
@@ -252,42 +250,6 @@ class DemoGame extends Component {
 							blendMode: 'SCENE',
 							scale: 0.6,
 							gravityY: 1000,
-							angle: { min: 0, max: 360 },
-							speed: 500,
-							quantity: 3,
-							lifespan: 1500,
-							stepRate: 200,
-							frequency: 100
-						}}
-					/>
-					<Particles
-						texture="coins"
-						frame="Gold_21.png"
-						animation={{
-							repeat: -1,
-							speed: 12,
-							generateFrameNames: {
-								prefix: 'Gold_',
-								suffix: '.png',
-								start: 21,
-								end: 30,
-								zeroPad: 2
-							},
-							generateFrameNamess: {
-								prefix: 'ruby_',
-								suffix: '',
-								start: 0,
-								end: 6,
-								zeroPad: 4
-							}
-						}}
-						config={{
-							radial: true,
-							x: 400,
-							y: 250,
-							blendMode: 'SCENE',
-							scale: 0.07,
-							gravityY: 3000,
 							angle: { min: 0, max: 360 },
 							speed: 500,
 							quantity: 3,
--- examples/demo/src/games/Playground/index.js
+++ examples/demo/src/games/Playground/index.js
@@ -23,14 +23,95 @@ var config = {
 };
 
 const assets = [
-	['atlas', 'coins', 'media/coins.png', 'media/coins.json'],
 	['atlas', 'gem', 'media/gems.png', gemsJson],
 	['atlas', 'flares', 'media/flares.png', flares],
-	['audio', 'background', 'media/music.mp3'],
-	['bitmapFont', 'bitfont', 'media/bitfont.png', 'media/bitfont.fnt'],
-	['image', 'background', 'media/sky.png'],
 	['image', 'crate', 'media/crate.png']
 ];
+
+window.tweensConfig = [
+	{
+		repeat: 0,
+		duration: 1000,
+		ease: 'Sine.easeIn',
+		props: {
+			x: {
+				from: 0,
+				value: 1000
+			}
+		}
+	},
+	{
+		loop: 3,
+		duration: 1000,
+		ease: 'Linear',
+		props: {
+			x: {
+				from: -1000,
+				value: 1000
+			}
+		},
+
+		onRepeat: (...args) => console.log('onRepeat', args),
+		onLoop: (...args) => console.log('onLoop', args)
+	},
+	{
+		repeat: 0,
+		duration: 1000,
+		ease: 'Sine.easeOut',
+		props: {
+			x: {
+				from: -1000,
+				value: 0
+			}
+		}
+	}
+];
+
+window.stopTween = false;
+window.tw = null;
+window.order = 0;
+window.start = (force) => {
+	if (force) stopTween = false;
+	if (stopTween) return;
+
+	const config = {
+		...tweensConfig[order]
+	};
+
+	order++;
+
+	config.props = Object.entries(config.props).reduce((acc, [key, prop]) => {
+		console.log(key, prop, { ...container.props, [key]: prop.from });
+
+		const from = prop.from;
+		prop = { ...prop };
+
+		delete prop.from;
+
+		if (typeof from !== undefined) {
+			container.instance[key] = from;
+		}
+
+		acc[key] = prop;
+
+		return acc;
+	}, {});
+
+	console.log(order, container.instance.x);
+	if (order >= tweensConfig.length) {
+		order = 0;
+	}
+
+	tw = sc.add.tween({
+		targets: container.instance,
+		...config,
+		onComplete: () => start()
+	});
+};
+
+window.stop = () => {
+	stopTween = true;
+};
 
 class PlaygroundGame extends Component {
 	state = {
@@ -52,42 +133,19 @@ class PlaygroundGame extends Component {
 		return (
 			<Game config={config}>
 				<Scene name="scene" assets={assets} active>
-					<Particles
-						texture="coins"
-						frame="Gold_21.png"
-						animation={{
-							repeat: -1,
-							speed: 12,
-							generateFrameNames: {
-								prefix: 'Gold_',
-								suffix: '.png',
-								start: 21,
-								end: 30,
-								zeroPad: 2
-							},
-							generateFrameNamess: {
-								prefix: 'ruby_',
-								suffix: '',
-								start: 0,
-								end: 6,
-								zeroPad: 4
-							}
-						}}
-						config={{
-							radial: true,
-							x: 400,
-							y: 250,
-							blendMode: 'SCENE',
-							scale: 0.07,
-							gravityY: 3000,
-							angle: { min: 0, max: 360 },
-							speed: 500,
-							quantity: 3,
-							lifespan: 1500,
-							stepRate: 200,
-							frequency: 100
-						}}
-					/>
+					<Container x={10} y={0}>
+						<Image texture="gem" frame="prism_0002" x={0} y={30} />
+						<Image texture="gem" frame="square_0002" x={70} y={30} />
+						<Image texture="gem" frame="ruby_0000" x={2 * 70} y={30} />
+						<Image texture="gem" frame="diamond_0000" x={3 * 70} y={30} />
+						<Image texture="gem" frame="prism_0002" x={4 * 70} y={30} />
+						<Image texture="gem" frame="square_0002" x={5 * 70} y={30} />
+						<Image texture="gem" frame="ruby_0000" x={6 * 70} y={30} />
+						<Image texture="gem" frame="diamond_0000" x={7 * 70} y={30} />
+						<Image texture="gem" frame="prism_0002" x={8 * 70} y={30} />
+						<Image texture="gem" frame="square_0002" x={9 * 70} y={30} />
+						<Image texture="gem" frame="ruby_0000" x={10 * 70} y={30} />
+					</Container>
 				</Scene>
 			</Game>
 		);
